7201e0ed2839e6750887f4f7f0acd147dfd07997
Nathan James <nathanjms@outlook.com>
2021-03-29 20:51:07 +0100

    Undo the conversion to ES6 as it stops working in browser 'require is not defined'

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import express from "express";
-import pool from "../config";
-const cors = require("cors");
+const express = require("express");
 
+const cors = require("cors");
+const { pool } = require("./config");
 const app = express();
 
 app.use(express.json());
